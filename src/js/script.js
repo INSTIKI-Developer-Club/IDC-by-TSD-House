d24b4bab3d85030b2c1318657f1034173b2d1733
AdiSan103 <arwinsmith01@gmail.com>
2023-06-27 11:37:10 +0800

    section 1 and section 2

---
+++ src/js/script.js
@@ -0,0 +1,15 @@
+// toggle dark mode
+
+// aos js
+AOS.init();
+
+// navbar
+let hamburgerMenu = document.querySelector(".hamburger-menu");
+let navbarItems = document.querySelector(".navbar-items");
+hamburgerMenu.addEventListener("click", () => {
+  if (navbarItems.classList.contains("-translate-y-[150%]")) {
+    navbarItems.classList.remove("-translate-y-[150%]");
+  } else {
+    navbarItems.classList.add("-translate-y-[150%]");
+  }
+});
