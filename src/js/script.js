bac73dd2f5b295bacb7690e673a55804d35247f8
AdiSan103 <arwinsmith01@gmail.com>
2023-07-12 07:49:51 +0800

    section 6 & 7

--- src/js/script.js
+++ src/js/script.js
@@ -13,3 +13,24 @@ hamburgerMenu.addEventListener("click", () => {
     navbarItems.classList.add("-translate-y-[150%]");
   }
 });
+
+// swiper js
+var swiper = new Swiper(".mySwiper-section6", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    clickable: true,
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    900: {
+      slidesPerView: 3,
+    },
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+});
