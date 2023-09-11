64132c42bd519008d83624e9d2bd06e059150436
AdiSan103 <arwinsmith01@gmail.com>
2023-09-11 11:23:35 +0800

    feat:beta

---
+++ README.md
@@ -0,0 +1,5 @@
+ npx tailwindcss -i ./src/css/input.css -o ./src/dist/output.css --watch
+
+ # Optimizing for production
+
+ npx tailwindcss -o build.css --minify
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        // color style 🌈
+        color1: "#A01C20",
+        color2: "#ED1F24",
+        color3: "#FBAD18",
+        color4: "#F6E016",
+        color5: "#000000",
+        color6: "#0B1A54",
+        color7: "#FEF4ED",
+        color8: "#FFFFFF",
+        color9: "#E2E2E2",
+        color10: "#858A8F",
+      },
+    },
+  },
+  plugins: [],
+};
